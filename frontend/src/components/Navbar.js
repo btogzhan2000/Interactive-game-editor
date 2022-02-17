8bcfcbd6a906ae54e667c852aeff110a49a1dedd
Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>
2022-02-17 15:59:42 +0600

    Added redactor

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { Button, Menu, Layout, Breadcrumb } from 'antd';
+import { Input } from 'antd';
+import './Navbar.css'
+import { Form} from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import "antd/dist/antd.css";
 import "../App.css"
 import { PlusCircleOutlined, FileTextOutlined } from '@ant-design/icons';
-
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
-
+const { TextArea } = Input;
 const Navbar = () => {
     const [pages, setPages] = useState([
         {
@@ -39,7 +42,9 @@ const Navbar = () => {
         }
     ]);
     const [count, setCount] = useState(5);
-
+    const onChange = e => {
+        console.log('Change:', e.target.value);
+      };
     const addNewPage = () => {
         const newList = pages.concat({name: "Page " + count});
         setCount(count+1);
@@ -49,6 +54,27 @@ const Navbar = () => {
         //e.key()
         console.log(e);
     }
+    const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 4 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 20 },
+        },
+      };
+      const onFinish = values => {
+        console.log('Received values of form:', values);
+      };
+      const formItemLayoutWithOutLabel = {
+        wrapperCol: {
+          xs: { span: 24, offset: 0 },
+          sm: { span: 20, offset: 4 },
+        },
+      };
+      
+    
     let editor = (
         <div>
             <h1>PAGE NAME</h1>
@@ -102,7 +128,78 @@ const Navbar = () => {
                             minHeight: '81vh',
                         }}
                     >
-                        Editor component
+                         
+                         <TextArea showCount maxLength={100} style={{ height: 120 }} onChange={onChange} />
+                        
+
+
+
+                         <Form name="dynamic_form_item" {...formItemLayoutWithOutLabel} onFinish={onFinish} style={{ margin: 20 }}>
+                    <Form.List
+                       name="names"
+                        rules={[
+                           {
+                            validator: async (_, names) => {
+                            if (!names || names.length < 2) {
+                            return Promise.reject(new Error('At least 2 passengers'));
+                           }
+                         },
+                         },
+                       ]}
+                    >
+        {(fields, { add, remove }, { errors }) => (
+          <>
+            {fields.map((field, index) => (
+              <Form.Item
+                {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                label={index === 0 ? 'Choices' : ''}
+                required={false}
+                key={field.key}
+              >
+                <Form.Item
+                  {...field}
+                  validateTrigger={['onChange', 'onBlur']}
+                  rules={[
+                    {
+                      required: true,
+                      whitespace: true,
+                      message: "Please input Choice or delete this field.",
+                    },
+                  ]}
+                  noStyle
+                >
+                  <Input placeholder="" style={{ width: '60%' }} />
+                </Form.Item>
+                {fields.length > 1 ? (
+                  <MinusCircleOutlined
+                    className="dynamic-delete-button"
+                    onClick={() => remove(field.name)}
+                  />
+                ) : null}
+              </Form.Item>
+            ))}
+            <Form.Item>
+              <Button
+                type="dashed"
+                onClick={() => add()}
+                style={{ width: '60%' }}
+                icon={<PlusOutlined />}
+              >
+                Add Choice
+              </Button>
+              
+              <Form.ErrorList errors={errors} />
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+
                     </Content>
 
                 </Layout>
@@ -113,3 +210,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
